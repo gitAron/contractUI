46223d0a2a4fcffbe42b3654bc93371e6aabf787
Hasreiter Johannes <johannes.hasreiter@msg-systems.com>
2016-08-12 06:52:47 +0000

    Erster funktionierender Prototyp

---
+++ webapp/Component.js
@@ -0,0 +1,54 @@
+jQuery.sap.declare("sap.ui.demo.myFiori.Component");
+
+sap.ui.core.UIComponent.extend("sap.ui.demo.myFiori.Component", {
+
+	createContent: function() {
+
+		// create root view
+		var oView = sap.ui.view({
+			id: "app",
+			viewName: "sap.ui.demo.myFiori.view.App",
+			type: "JS",
+			viewData: {
+				component: this
+			}
+		});
+
+		var oParameters = {}; // username : "<username>", password : "<password>"
+		$.ajax({
+			dataType: "json",
+			data: oParameters,
+			type: "GET",
+			url: "/findLoss", //see neo-app.json destination and destinations in Cloud Cockpit
+			//Debugging reasons
+			//error: function(ts) {alert(ts.responseText)},
+			success: function(data) {
+
+				// set data model on root view
+				var oModel = new sap.ui.model.json.JSONModel({
+					Loss: data
+				});
+				oView.setModel(oModel);
+			}
+
+		});
+
+		// set i18n model
+		var i18nModel = new sap.ui.model.resource.ResourceModel({
+			bundleUrl: "i18n/messageBundle.properties"
+		});
+		oView.setModel(i18nModel, "i18n");
+
+		// set device model
+		var deviceModel = new sap.ui.model.json.JSONModel({
+			isPhone: jQuery.device.is.phone,
+			listMode: (jQuery.device.is.phone) ? "None" : "SingleSelectMaster",
+			listItemType: (jQuery.device.is.phone) ? "Active" : "Inactive"
+		});
+		deviceModel.setDefaultBindingMode("OneWay");
+		oView.setModel(deviceModel, "device");
+
+		// done
+		return oView;
+	}
+});
