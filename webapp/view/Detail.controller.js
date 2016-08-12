51abcaf5f9bc6671cabcd04d34ec6a76534d243a
Aron Brinckmann <aron.brinckmann@msg-systems.com>
2016-08-12 10:45:37 +0000

    handleLineItemPress Funktion gelöscht. Hat keinen Effekt

--- webapp/view/Detail.controller.js
+++ webapp/view/Detail.controller.js
@@ -4,9 +4,7 @@ jQuery.sap.require("sap.m.MessageToast");
 
 sap.ui.controller("sap.ui.demo.myFiori.view.Detail", {
 
-	handleNavButtonPress : function (evt) {
-		this.nav.back("Master");
-	},
+
 	
 	handleApprove : function (evt) {
 
@@ -25,10 +23,7 @@ sap.ui.controller("sap.ui.demo.myFiori.view.Detail", {
 			
 			bundle.getText("ApproveDialogTitle")
 		);
-	},
-	
-	handleLineItemPress : function (evt) {
-		var context = evt.getSource().getBindingContext();
-		this.nav.to("LineItem", context);
 	}
+	
+
 });
